10abd6f757d03d0c483eb11014975d905c43139e
Aleksandr Osokin <sanvovanych@gmail.com>
2020-08-26 01:45:11 +0300

    functions of creating folders and files. project file content

--- index.js
+++ index.js
@@ -16,16 +16,38 @@ const color = {
 
 const bold = clc.bold
 
-// Create folders
 let projectLocation = config.project.location
 if (projectLocation[0] === '~') {
 	projectLocation = projectLocation.replace('~', os.homedir())
 }
+let projectName = config.project.name
 
-let filePath = path.join(projectLocation, config.project.name, config.project.files[0])
-// let filePath = path.join('~/', config.project.name, config.project.files[0])
-fs.createFileSync(filePath, (err) => {
-	console.log( color.error(err) );
+// Create folders
+let folders = config.project.folders
+folders.forEach( folder => {
+	fs.mkdirsSync(path.join(projectLocation, projectName, folder), (err) => {
+		console.log( color.error(err) )
+	})
+})
+
+// Create files
+var files = []
+
+config.project.files.forEach(file => {
+	file = path.join(projectLocation, projectName, file)
+	files.push(file)
+})
+
+let editorProjectFile = path.join(projectLocation, projectName, projectName + '.sublime-project')
+files.push(editorProjectFile)
+
+let filePath = path.join(projectLocation, projectName, config.project.files[0])
+
+// let filePath = path.join('~/', projectName, config.project.files[0])
+files.forEach(file => {
+	fs.createFileSync(file, (err) => {
+		console.log( color.error(err) )
+	})
 })
 
 let accessContent = `## ftp
@@ -34,20 +56,45 @@ path:
 user:
 password:
 
-## database
+## Database
 host: localhost
 dbname:
 user:
 password:
-`;
+`
+
+let editorProjectContent = `{
+	"folders":
+	[
+		{
+			"path": "."
+		},
+		{
+			"path": "~/vhosts/${projectName}",
+			"name": "working-copy"
+		}
+	]
+}
+`
+
+fs.writeFileSync(editorProjectFile, editorProjectContent, (err) => {
+	console.log( color.error(err) );
+})
+
+console.log(
+	'\n' +
+	'File',
+	color.file(editorProjectFile),
+	'is created'
+)
 
 fs.writeFileSync(filePath, accessContent, (err) => {
 	console.log( color.error(err) );
 })
+
 console.log(
-	'\n' +
 	'File',
-	color.file(config.project.name + '/' + config.project.files[0]),
+	color.file(projectName + '/' + config.project.files[0]),
 	'is created'
 )
 
@@ -56,4 +103,3 @@ console.log(
 	'\n'
 )
 
-// Create files
